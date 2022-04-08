3e0e8afd46564491904c773b0fcbe47303d68b84
BoredTenYang158 <95093399+TenzinYangzom158@users.noreply.github.com>
2022-04-08 11:15:58 +0530

    Update Base.js

--- src/Base.js
+++ src/Base.js
@@ -5,7 +5,7 @@ const Base = ({children})=>{
     return <div>
         <header>
             <div className='row bg-danger m-0 p-1'>
-                <div className='col-4 bg-success'>Recruitz</div>
+                <div className='col-4 bg-success'>RecruitZ</div>
                 <div className='col-4 bg-light'>zz</div>
                 {children}
             </div>
