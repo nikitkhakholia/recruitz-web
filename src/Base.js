f040c08b4bba017409079f9708f554a6ea79a392
nikitkhakholia <khakholia.nk@gmail.com>
2022-04-08 11:10:24 +0530

    init

---
+++ src/Base.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './Home/Home';
+
+const Base = ({children})=>{
+    return <div>
+        <header>
+            <div className='row bg-danger m-0 p-1'>
+                <div className='col-4 bg-success'>Recruitz</div>
+                <div className='col-4 bg-light'>zz</div>
+                {children}
+            </div>
+        </header>
+    </div>
+}
+
+export default Base;
---
+++ src/Home/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Base from '../Base'
+
+const Home=() =>{
+  return (
+    <div>Home</div>
+  )
+}
+export default Home
